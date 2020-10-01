b0133e945a8a494ff4092e8cde2f6bb940440ec1
Frank <frank7300228@gmail.com>
2020-10-01 17:20:50 +0800

    Finish 3-32

--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -1,3 +1,14 @@
 import React from "react"
-import Enzyme, { shallow } from "enzyme"
+import { shallow } from "enzyme"
 import Congrats from "./Congrats"
+import { findByTestAttr } from "../test/testUtils"
+
+const setup = (props = {}) => {
+  return shallow(<Congrats {...props} />)
+}
+
+test("renders without error", () => {})
+
+test("render no text when `success` prop is false", () => {})
+
+test("render non-empty congrats message when `success` prop is true", () => {})
