37f53f729f435abb613d202e7e24ba7e98ab22cf
Frank <frank7300228@gmail.com>
2020-10-01 22:33:58 +0800

    Finish 3-34

--- src/Congrats.js
+++ src/Congrats.js
@@ -1,5 +1,15 @@
 import React from "react"
 
-export default () => {
-  return <div />
+export default (props) => {
+  if (props.success) {
+    return (
+      <div data-test="component-congrats">
+        <span data-test="congrats-message">
+          Congratulations! You guessed the word!
+        </span>
+      </div>
+    )
+  }
+
+  return <div data-test="component-congrats" />
 }
