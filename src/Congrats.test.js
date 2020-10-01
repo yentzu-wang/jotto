72b302ed5acdd30772ad1464967f016407f3d0b8
Frank <frank7300228@gmail.com>
2020-10-01 22:28:58 +0800

    Finish 3-33

--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -7,8 +7,20 @@ const setup = (props = {}) => {
   return shallow(<Congrats {...props} />)
 }
 
-test("renders without error", () => {})
+test("renders without error", () => {
+  const wrapper = setup()
+  const component = findByTestAttr(wrapper, "component-congrats")
+  expect(component.length).toBe(1)
+})
 
-test("render no text when `success` prop is false", () => {})
+test("render no text when `success` prop is false", () => {
+  const wrapper = setup({ success: false })
+  const component = findByTestAttr(wrapper, "component-congrats")
+  expect(component.text()).toBe("")
+})
 
-test("render non-empty congrats message when `success` prop is true", () => {})
+test("render non-empty congrats message when `success` prop is true", () => {
+  const wrapper = setup({ success: true })
+  const message = findByTestAttr(wrapper, "congrats-message")
+  expect(message.text().length).not.toBe(0)
+})
