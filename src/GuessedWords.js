2bd6c233ae65598d70c59028d3a803e44df81468
Frank <frank7300228@gmail.com>
2020-10-03 00:34:41 +0800

    Finish 3-43

--- src/GuessedWords.js
+++ src/GuessedWords.js
@@ -5,6 +5,13 @@ const GuessedWords = (props) => {
   return <div></div>
 }
 
-GuessedWords.propTypes = {}
+GuessedWords.propTypes = {
+  guessedWords: PropTypes.arrayOf(
+    PropTypes.shape({
+      guessedWord: PropTypes.string.isRequired,
+      letterMatchCount: PropTypes.number.isRequired
+    })
+  ).isRequired
+}
 
 export default GuessedWords
