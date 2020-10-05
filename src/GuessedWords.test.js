b7fddcbb969dbfca7791b06d37d45f1e2470637b
Frank <frank7300228@gmail.com>
2020-10-05 19:06:03 +0800

    Finish 3-45

--- src/GuessedWords.test.js
+++ src/GuessedWords.test.js
@@ -16,6 +16,22 @@ test("does not throw warning with expected props", () => {
   checkProps(GuessedWords, defaultProps)
 })
 
-describe("if there are no words guessed", () => {})
+describe("if there are no words guessed", () => {
+  let wrapper
+
+  beforeEach(() => {
+    wrapper = setup({ guessWords: [] })
+  })
+
+  test("renders without error", () => {
+    const component = findByTestAttr(wrapper, "component-guessed-words")
+    expect(component.length).toBe(1)
+  })
+
+  test("renders instructions to guess a word", () => {
+    const instructions = findByTestAttr(wrapper, "guess-instructions")
+    expect(instructions.text().length).not.toBe(0)
+  })
+})
 
 describe("if there are words guessed", () => {})
