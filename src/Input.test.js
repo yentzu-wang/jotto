7d267469a7e7fa843f3b46a87173909030bd71b4
Frank <frank7300228@gmail.com>
2020-10-06 15:53:14 +0800

    Finish 5-61

--- src/Input.test.js
+++ src/Input.test.js
@@ -3,4 +3,22 @@ import { shallow } from "enzyme"
 import { findByTestAttr } from "../test/testUtils"
 import Input from "./Input"
 
-test("", () => {})
+describe("render", () => {
+  describe("word has not been guessed", () => {
+    test("renders component without error", () => {})
+
+    test("renders input box", () => {})
+
+    test("renders submit button", () => {})
+  })
+
+  describe("word has been guessed", () => {
+    test("renders component without error", () => {})
+
+    test("does not render input box", () => {})
+
+    test("does not render submit button", () => {})
+  })
+})
+
+describe("update state", () => {})
