372f9fbb23081808dafb300e31eec0d9ec3e9831
Frank <frank7300228@gmail.com>
2020-10-06 15:35:50 +0800

    Finish 5-57

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,5 +3,5 @@ export const actionTypes = {
 }
 
 export function correctGuess() {
-  return { type: actionTypes.CORRECT_GUESS }
+  return null
 }
---
+++ src/reducers/successReducer.test.js
@@ -0,0 +1,14 @@
+import successReducer from "./successReducer"
+import { actionTypes } from "../actions"
+
+test("returns default inital state of `false` when no action is passed", () => {
+  const newState = successReducer()
+  expect(newState).toBe(false)
+})
+
+test("returns state of true upon receiving an action of type `CORRECT_GUESS`", () => {
+  const newState = successReducer(undefined, {
+    type: actionTypes.CORRECT_GUESS
+  })
+  expect(newState).toBe(true)
+})
