9421f91c9fe552f033f6bd29a0ff8c62bd6dd876
Frank <frank7300228@gmail.com>
2020-10-07 14:41:48 +0800

    Finish 5-63

--- src/Input.test.js
+++ src/Input.test.js
@@ -1,10 +1,11 @@
 import React from "react"
 import { shallow } from "enzyme"
-import { findByTestAttr } from "../test/testUtils"
+import { findByTestAttr, storeFactory } from "../test/testUtils"
 import Input from "./Input"
 
 const setup = (initialState = {}) => {
-  const wrapper = shallow(<Input />)
+  const store = storeFactory(initialState)
+  const wrapper = shallow(<Input store={store} />)
   console.log(wrapper.debug())
 }
 
--- test/testUtils.js
+++ test/testUtils.js
@@ -1,4 +1,10 @@
+import { createStore } from "redux"
 import checkPropTypes from "check-prop-types"
+import rootReducer from "../src/reducers"
+
+export const storeFactory = (initialState) => {
+  return createStore(rootReducer, initialState)
+}
 
 export const findByTestAttr = (wrapper, val) => {
   return wrapper.find(`[data-test='${val}']`)
