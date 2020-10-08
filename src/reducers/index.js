bd39325dec82e8258452531abd97b942fb10581b
Frank <frank7300228@gmail.com>
2020-10-08 16:44:22 +0800

    Finish 6-74

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,5 @@
 import { combineReducers } from "redux"
 import success from "./successReducer"
+import guessWords from "./guessedWordsReducer"
 
-export default combineReducers({ success })
+export default combineReducers({ success, guessWords })
