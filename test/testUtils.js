52f9ce6a37420fca581dea56847842cefa24e297
Frank <frank7300228@gmail.com>
2020-10-08 16:33:05 +0800

    Finish 6-72

--- test/testUtils.js
+++ test/testUtils.js
@@ -1,9 +1,11 @@
-import { createStore } from "redux"
+import { createStore, applyMiddleware } from "redux"
 import checkPropTypes from "check-prop-types"
 import rootReducer from "../src/reducers"
+import { middlewares } from "../src/configureStore"
 
 export const storeFactory = (initialState) => {
-  return createStore(rootReducer, initialState)
+  const createStoreWithMiddleware = applyMiddleware(...middlewares)(createStore)
+  return createStoreWithMiddleware(rootReducer, initialState)
 }
 
 export const findByTestAttr = (wrapper, val) => {
