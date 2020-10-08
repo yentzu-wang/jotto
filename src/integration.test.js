81bd9b88bcdb2cc0641208038e4aab08ef06da51
Frank <frank7300228@gmail.com>
2020-10-08 16:52:36 +0800

    Finish 6-75

--- src/integration.test.js
+++ src/integration.test.js
@@ -2,8 +2,33 @@ import { storeFactory } from "../test/testUtils"
 import { guessWord } from "./actions"
 
 describe("guessWord action dispatcher", () => {
+  const secretWord = "party"
+  const unsuccessfulGuess = "train"
+
   describe("no guessed words", () => {
-    test("updates state correctly for unsuccessful guess", () => {})
+    let store
+    const initialState = { secretWord }
+
+    beforeEach(() => {
+      store = storeFactory(initialState)
+    })
+
+    test("updates state correctly for unsuccessful guess", () => {
+      store.dispatch(guessWord(unsuccessfulGuess))
+      const expectedState = {
+        ...initialState,
+        success: false,
+        guessedWords: [
+          {
+            guessedWord: unsuccessfulGuess,
+            letterMatchCount: 3
+          }
+        ]
+      }
+      const newState = store.getState()
+
+      expectedState(newState).toEqual(expectedState)
+    })
 
     test("updates state correctly for successful guess", () => {})
   })
