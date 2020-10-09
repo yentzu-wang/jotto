0fec91e8c9e216d79cd4d268735bebf1563769ca
Frank <frank7300228@gmail.com>
2020-10-09 15:53:59 +0800

    Finish 7-82

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,10 @@
+import axios from "axios"
 import { getLetterMatchCount } from "../helpers"
 
 export const actionTypes = {
   CORRECT_GUESS: "CORRECT_GUESS",
-  GUESS_WORD: "GUESS_WORD"
+  GUESS_WORD: "GUESS_WORD",
+  SET_SECRET_WORD: "SET_SECRET_WORD"
 }
 
 export const guessWord = (guessedWord) => {
@@ -18,5 +20,11 @@ export const guessWord = (guessedWord) => {
     if (guessedWord === secretWord) {
       dispatch({ type: actionTypes.CORRECT_GUESS })
     }
+  }
+}
+
+export const getSecretWord = () => {
+  return (dispatch) => {
+    axios.get("http://localhost:3030")
   }
 }
