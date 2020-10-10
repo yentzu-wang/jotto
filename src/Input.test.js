726bd5e1d6ad45a2bafd0f9c7ba3c4d2b8ee3181
Frank <frank7300228@gmail.com>
2020-10-10 20:09:41 +0800

    Finish 8-93

--- src/Input.test.js
+++ src/Input.test.js
@@ -1,8 +1,7 @@
 import React from "react"
 import { shallow } from "enzyme"
 import { findByTestAttr, storeFactory } from "../test/testUtils"
-import Input from "./Input"
-import { guessWord } from "./actions"
+import Input, { UnconnectedInput } from "./Input"
 
 const setup = (initialState = {}) => {
   const store = storeFactory(initialState)
@@ -74,5 +73,27 @@ describe("redux props", () => {
     const wrapper = setup()
     const guessWordProp = wrapper.instance().props.guessWord
     expect(guessWordProp).toBeInstanceOf(Function)
+  })
+})
+
+describe("`guessWord` action creator call", () => {
+  test("call `guessWord` when button is clicked", () => {
+    const guessWordMock = jest.fn()
+
+    const props = {
+      guessWord: guessWordMock
+    }
+
+    // set up app component with guessWordMock as the guessWord prop
+    const wrapper = shallow(<UnconnectedInput {...props} />)
+
+    // simulate clicked
+    const submitButton = findByTestAttr(wrapper, "submit-button")
+    submitButton.simulate("click")
+
+    // check to see if mock ran
+    const guessWordCallCount = guessWordMock.mock.calls.length
+
+    expect(guessWordCallCount).toBe(1)
   })
 })
